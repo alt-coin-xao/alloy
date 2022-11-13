141b155a0fe75a7008c0772f162c8d7c8a71ffb4
glencjones <glencjones>
2022-11-13 18:20:09 +0000

    updated testnet scripts add-alloy

--- scripts/testnet/start-alloyd.sh
+++ scripts/testnet/start-alloyd.sh
@@ -10,4 +10,4 @@ WALLET_ADDRESS=$(cat $CWD/${walletname}.address)
 
 echo "Wallet Address ${WALLET_ADDRESS}"
 
-$CWD/../../build/src/alloyd --testnet --fee-address=${WALLET_ADDRESS} --genesis-block-reward-address=${WALLET_ADDRESS} --rpc-bind-port=1911 --p2p-bind-port=1910 $* 
+$CWD/../../build/src/alloyd --log-level=4 --testnet --data-dir=$CWD/.alloy-testnet --fee-address=${WALLET_ADDRESS} --genesis-block-reward-address=${WALLET_ADDRESS} --rpc-bind-port=1911 --p2p-bind-port=1910 $* 
